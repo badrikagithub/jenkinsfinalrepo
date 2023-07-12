8f1cc20dbd00c9f34fa291274e8ff8fee99641b6
badrikagithub <badrika.454@gmail.com>
2023-07-12 12:03:06 +0530

    second commit

---
+++ jenkins/src/test/java/JenkinsPractice/Browser.java
@@ -0,0 +1,14 @@
+package JenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class Browser 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("Execution done by Jenkins");
+	}
+	
+
+}
---
+++ jenkins/src/test/java/JenkinsPractice/Browsertest2.java
@@ -0,0 +1,13 @@
+package JenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class Browsertest2 
+{
+	@Test
+	public void m2()
+	{
+		System.out.println("execution not done by jenkins");
+	}
+
+}
